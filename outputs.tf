bc9c9ccfe887f16a69c4f1293793cd97b67b87bf
Michael Prestin <mprestin@mikep-mac.lan>
2021-03-03 15:38:23 -0800

    modified:   outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 
 output "BASTION_IP" { value = module.bastion.public_ip }
 output "SSH_KEY_INFO" { value = var.provide_ssh_key ? "SSH Key Provided by user" : "See below for generated SSH private key." }
-output "SSH_PRIVATE_KEY" { value = var.provide_ssh_key ? "SSH Key Provided by user" : tls_private_key.oke_ssh_key.private_key_pem }
+output "SSH_PRIVATE_KEY" { value = var.provide_ssh_key ? "SSH Key Provided by user" : tls_private_key.ssh_key.private_key_pem }
 
